2fc81c60d5d72c458e2da315a0f3a38751d6d42b
The-Doctor-What <hitv666@gmail.com>
2026-06-09 22:37:13 +0300

    fix: Исправлено отображение изображений

--- src/js/data.js
+++ src/js/data.js
@@ -6,7 +6,7 @@ export const courses = [
         badgeText: "Marketing",
         price: 100,
         author: "Jerome Bell",
-        image: "/src/assets/course-1.png"
+        image: "/course-1.png"
     },
     {
         id: 2,
@@ -15,7 +15,7 @@ export const courses = [
         badgeText: "Management",
         price: 480,
         author: "Marvin McKinney",
-        image: "/src/assets/course-2.png"
+        image: "/course-2.png"
     },
     {
         id: 3,
@@ -24,7 +24,7 @@ export const courses = [
         badgeText: "HR & Recruiting",
         price: 200,
         author: "Leslie Alexander Li",
-        image: "/src/assets/course-3.png"
+        image: "/course-3.png"
     },
     {
         id: 4,
@@ -33,7 +33,7 @@ export const courses = [
         badgeText: "Marketing",
         price: 530,
         author: "Kristin Watson",
-        image: "/src/assets/course-4.png"
+        image: "/course-4.png"
     },
     {
         id: 5,
@@ -42,7 +42,7 @@ export const courses = [
         badgeText: "Design",
         price: 500,
         author: "Guy Hawkins",
-        image: "/src/assets/course-5.png"
+        image: "/course-5.png"
     },
     {
         id: 6,
@@ -51,7 +51,7 @@ export const courses = [
         badgeText: "Management",
         price: 400,
         author: "Dianne Russell",
-        image: "/src/assets/course-6.png"
+        image: "/course-6.png"
     },
     {
         id: 7,
@@ -60,7 +60,7 @@ export const courses = [
         badgeText: "Development",
         price: 600,
         author: "Brooklyn Simmons",
-        image: "/src/assets/course-7.png"
+        image: "/course-7.png"
     },
     {
         id: 8,
@@ -69,7 +69,7 @@ export const courses = [
         badgeText: "HR & Recruiting",
         price: 150,
         author: "Kathryn Murphy",
-        image: "/src/assets/course-8.png"
+        image: "/course-8.png"
     },
     {
         id: 9,
@@ -78,6 +78,6 @@ export const courses = [
         badgeText: "Design",
         price: 240,
         author: "Cody Fisher",
-        image: "/src/assets/course-9.png"
+        image: "/course-9.png"
     }
 ];
